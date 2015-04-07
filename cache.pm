6ed0691a80f720b92bd30e0a8ca8dee92ec72e52
Scott Baker <scott@perturb.org>
2015-04-07 15:40:00 -0700

    Store the key in the cache also

--- cache.pm
+++ cache.pm
@@ -8,17 +8,17 @@ sub cache {
 	my $file = md5_hex($key);                          # Hash keys for directory load balancing
 	my $path = "/tmp/perl-cache/" . substr($file,0,3); # 4096 cache dirs
 
-	if ($key && @_ == 1) {                                             # ** Get **
-		if (!-r "$path/$file") { return undef; }                       # Not on disk
-		my $i = retrieve("$path/$file");                               # Fetch the cache from disk
-		if ($i->{'expires'} < time()) { cache($key,''); }              # Expired, delete the file
-		else { return $i->{'data'};	}                                  # Return the data structure
-	} elsif ($key && $value) {                                         # ** Set **
-		my $time = $time || (time + 86400);                            # Default expiration
-		make_path($path);                                              # Build the cache dir
-		return store({'expires'=>$time,'data'=>$value},"$path/$file"); # Store on disk
-	} elsif ($key && !defined($value)) {                               # ** Delete **
-		return unlink("$path/$file");                                  # Remove cache from disk
+	if ($key && @_ == 1) {                                                         # ** Get **
+		if (!-r "$path/$file") { return undef; }                                   # Not on disk
+		my $i = retrieve("$path/$file");                                           # Fetch the cache from disk
+		if ($i->{'expires'} < time()) { cache($key,''); }                          # Expired, delete the file
+		else { return $i->{'data'};	}                                              # Return the data structure
+	} elsif ($key && $value) {                                                     # ** Set **
+		my $time = $time || (time + 86400);                                        # Default expiration
+		make_path($path);                                                          # Build the cache dir
+		return store({'key'=>$key,'expires'=>$time,'data'=>$value},"$path/$file"); # Store on disk
+	} elsif ($key && !defined($value)) {                                           # ** Delete **
+		return unlink("$path/$file");                                              # Remove cache from disk
 	}
 }
 
