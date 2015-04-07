f4517aae7ce1b36c211c6687a25b094a6a7f317f
Scott Baker <scott@perturb.org>
2015-04-07 14:59:31 -0700

    Add an example Perl implementation

---
+++ example.pl
@@ -0,0 +1,27 @@
+######################################################################
+# Super simple disk based cached using only Perl core modules
+######################################################################
+# Scott Baker - 2015-04-07
+#
+# Similar concept to memcache: get/set/delete. For a more full
+# featured Perl cache module, look at Cache::Cache. This was designed
+# to be a single function, which be copy/pasted in to existing code
+######################################################################
+
+use cache;
+use strict;
+
+my $ok   = 0;
+my $name = '';
+
+# Store an item in the cache indefinitely
+$ok = cache("name", "scott");
+
+# Store an item in the cache for 60 seconds
+$ok = cache("name", "scott", time() + 60);
+
+# Fetch an item from the cache
+$name = cache("name");
+
+# Delete an item from the cache
+$ok = cache("name",undef);
